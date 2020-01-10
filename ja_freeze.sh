8f4b4e4800594922cc63e3d15db9405ee702e0c6
Johnny_Wick <wolfmib@gmail.com>
2020-01-10 11:01:20 +0800

    add_freeeze_sheel_et_requrement_txt

---
+++ ja_freeze.sh
@@ -0,0 +1,8 @@
+
+echo "[Jason]: Start to crate requirements.txt"
+pip3 freeze > requirements.txt
+
+echo "[Jason]: Finish.. you can install this in other device by typing"
+echo "----------------------------------------"
+echo "pip or pip3 install -r requirements.txt"
+echo "----------------------------------------"
